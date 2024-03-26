4d6296b591158d6d1ecaa4fb6202f1a91bcc40d3
Rashad Raul <rashad.tgr@gmail.com>
2024-03-27 01:26:03 +0400

    Categories initial state

---
+++ src/main/java/org/raul/receipesweb/controller/CategoryController.java
@@ -0,0 +1,29 @@
+package org.raul.receipesweb.controller;
+
+import org.raul.receipesweb.model.Category;
+import org.raul.receipesweb.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categoryList = categoryService.getAllCategories();
+        return ResponseEntity.ok(categoryList);
+    }
+}
