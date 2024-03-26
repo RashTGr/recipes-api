44389d1b408b1635318f76096be5b3ae8f92e005
Rashad Raul <rashad.tgr@gmail.com>
2024-03-27 01:01:31 +0400

    Initial commit wiht setup

---
+++ src/main/java/org/raul/receipesweb/ReceipeswebApplication.java
@@ -0,0 +1,13 @@
+package org.raul.receipesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipeswebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceipeswebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/raul/receipesweb/ReceipeswebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.raul.receipesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipeswebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
