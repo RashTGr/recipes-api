01abecd23f13a5238baced3689d133e9d772896d
Rashad Raul <rashad.tgr@gmail.com>
2024-03-27 04:02:41 +0400

    Remove redundant imports

--- src/main/java/org/raul/receipesweb/controller/CategoryController.java
+++ src/main/java/org/raul/receipesweb/controller/CategoryController.java
@@ -3,7 +3,6 @@ package org.raul.receipesweb.controller;
 import lombok.AllArgsConstructor;
 import org.raul.receipesweb.model.Category;
 import org.raul.receipesweb.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/org/raul/receipesweb/exception/GlobalExceptionHandler.java
+++ src/main/java/org/raul/receipesweb/exception/GlobalExceptionHandler.java
@@ -4,11 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
