100b8c59e4247a8303e61c6d54d0039cc2678d11
Rashad Raul <rashad.tgr@gmail.com>
2024-05-11 14:39:39 +0400

    Impl logging to category service

--- src/main/java/org/raul/receipesweb/service/CategoryService.java
+++ src/main/java/org/raul/receipesweb/service/CategoryService.java
@@ -1,5 +1,6 @@
 package org.raul.receipesweb.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.raul.receipesweb.exception.ResourceNotFoundException;
 import org.raul.receipesweb.model.Category;
 import org.raul.receipesweb.repository.CategoryRepository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Slf4j
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
@@ -20,15 +22,18 @@ public class CategoryService {
 
     // implement DTO logic here!!!
     public List<Category> getAllCategories() {
+        log.info("Fetching categories from the db");
         return categoryRepository.findAll();
     }
 
     public Category getCategoryById(Long id) {
+        log.info("Fetching category by id {}", id);
         return categoryRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Couldn't found category with id :: " + id));
     }
 
     public Category addCategory(Category category) {
+        log.info("Adding new category: {}", category.getName());
         return categoryRepository.save(category);
     }
 
@@ -37,13 +42,15 @@ public class CategoryService {
                 .orElseThrow(() -> new ResourceNotFoundException("Couldn't found category with id :: " + id));
 
         category.setName(categoryName.getName());
+        log.info("Updated category by id {} and name: {}", id, categoryName.getName());
         return categoryRepository.save(category);
     }
 
     public void deleteCategory(Long id) {
+        log.debug("Deleting category id {} from db", id);
         Category category = categoryRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Couldn't found category with id :: " + id));
-
+        log.info("Deleted category {} ", id);
         categoryRepository.delete(category);
     }
 }
